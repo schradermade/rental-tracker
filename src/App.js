3092d4d9537190b2c2a03d381aa541bded3e44c9
Nathan Schrader <schradermade@Nathans-MBP.netgear.com>
2021-03-08 13:01:16 -0800

    add unsettled(), settled() methods

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       {
         'id' : '1',
         'Address' : '12345 SW Main St, Tualatin, Oregon 97062',
+        'Status' : false,
         'Rent' : false,
         'Garbage' : false
       }
@@ -24,6 +25,20 @@ class App extends Component {
     let rental = [...this.state.rentals].filter (i => i.id ===id);
     let url = rental[0].RentalTable;
     window.open(url);
+  }
+
+  unsettled(id) {
+    let rental = this.state.rentals.filter (i => i.id ===id);
+    rental[0].Status = false;
+    let updatedRentals = [...this.state.rentals];
+    this.setState({ rentals : updatedRentals });
+  }
+
+  settled(id) {
+    let rental = this.state.rentals.filter (i => i.id ===id);
+    rental[0].Status = true;
+    let updatedRentals = [...this.state.rentals];
+    this.setState({ rentals : updatedRentals });
   }
 
 }
